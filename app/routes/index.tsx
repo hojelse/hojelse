9cb5147e88e06978fbc9035cd8bbf2f4d2dd5a8b
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2022-05-09 16:47:47 +0200

    Add pinnedItems, graphql request to api.github.com

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,31 +1,63 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { GraphQLClient, gql } from "graphql-request";
+
+interface Repository {
+  name: string
+}
+
+interface GetPinnedReposQueryInterface {
+  user: {
+    name: string,
+    pinnedItems: {
+      nodes: Repository[]
+    }
+  }
+}
+
+const GetPinnedReposQuery = gql`
+  {
+    user(login: "hojelse") {
+      name,
+      pinnedItems(first: 6, types: REPOSITORY) {
+        nodes {
+          ... on Repository {
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const loader : LoaderFunction = async (params) => {
+  const pat = process.env["OCTOKIT_PAT"]
+  
+  const githubapi = new GraphQLClient(
+    "https://api.github.com/graphql",
+    { headers: { Authorization: `bearer ${pat}` } }
+  );
+
+  const resp = await githubapi.request(GetPinnedReposQuery);
+
+  return json<GetPinnedReposQueryInterface>(resp)
+}
+
 export default function Index() {
+  const data = useLoaderData<GetPinnedReposQueryInterface>();
+  const repos = data.user.pinnedItems.nodes
+  console.dir(data)
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
+    <div>
+      <h1>{data.user.name}</h1>
+      <h2>My projects</h2>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {
+          repos.map((repo) => {
+            return <li key={repo.name}>{repo.name}</li>
+          })
+        }
       </ul>
     </div>
   );
